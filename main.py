41b960d8428daf3a951f55597996e86da80f24c4
rishabh-3112 <lataatlondon@gmail.com>
2026-01-16 18:29:52 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# ating different variables
+x = 5
+
+n = "John"
+print(x)
+print(n)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("Enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
